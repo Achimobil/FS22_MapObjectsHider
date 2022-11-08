d90df9f6d2e9c00df1e6484c690e4f37eddb1f64
Achim Kuntz <achimobil@hotmail.com>
2022-11-08 17:39:35 +0100

    Ausblenden von normalen objekten geht schon mal

--- mapObjectsHider.lua
+++ mapObjectsHider.lua
@@ -14,6 +14,13 @@ MapObjectsHider.metadata = {
 	info = "Das verändern und wiederöffentlichen, auch in Teilen, ist untersagt und wird abgemahnt."
 };
 MapObjectsHider.isInit = false;
+MapObjectsHider.debug = true;
+MapObjectsHider.md5 = not MapObjectsHider.debug
+MapObjectsHider.hideConfirmEnabled = true
+MapObjectsHider.sellConfirmEnabled = true
+MapObjectsHider.deleteSplitShapeConfirmEnabled = true
+MapObjectsHider.guiShowHelpEnabled = true
+MapObjectsHider.hiddenObjects = {}
 
 function MapObjectsHider:init()
 	MapObjectsHider.isInit = true;
@@ -22,11 +29,228 @@ function MapObjectsHider:init()
 		return;
 	end
 	Logging.info("%s - init (Version: %s)", self.metadata.name, self.metadata.version)
+	
+	-- Bind PlayerExtension
+	-- alte funktion speichern, da keine Klassenfunktion
+	if Player.originalNew == nil then
+		Player.originalNew = Player.new
+	end
+	Player.new = Utils.overwrittenFunction(Player.new, PlayerExtension.new)
+	
+	
+	
+	Player.updateTick = Utils.overwrittenFunction(Player.updateTick, PlayerExtension.updateTick)
+	Player.update = Utils.overwrittenFunction(Player.update, PlayerExtension.update)
+	Player.updateActionEvents = Utils.overwrittenFunction(Player.updateActionEvents, PlayerExtension.updateActionEvents)
+
+	if Player.raycastCallback == nil then
+		Player.raycastCallback = PlayerExtension.raycastCallback
+	end
+
+	if Player.hideObjectActionEvent == nil then
+		Player.hideObjectActionEvent = PlayerExtension.hideObjectActionEvent
+	end
+
+	if Player.hideObjectDialogCallback == nil then
+		Player.hideObjectDialogCallback = PlayerExtension.hideObjectDialogCallback
+	end
+
+	if Player.sellObjectDialogCallback == nil then
+		Player.sellObjectDialogCallback = PlayerExtension.sellObjectDialogCallback
+	end
+
+	if Player.deleteSplitShapeDialogCallback == nil then
+		Player.deleteSplitShapeDialogCallback = PlayerExtension.deleteSplitShapeDialogCallback
+	end
+
+	if Player.showHiddenObjectsListActionEvent == nil then
+		Player.showHiddenObjectsListActionEvent = PlayerExtension.showHiddenObjectsListActionEvent
+	end
+	
 end
+
 function MapObjectsHider:loadMap(filename)
 	Logging.info("%s:loadMap(%s)", self.metadata.name, filename)
 	-- init on first call
 	if not MapObjectsHider.isInit then MapObjectsHider:init(); end
+end
+
+---@param objectId integer
+---@return table
+function MapObjectsHider:getObjectDebugInfo(objectId)
+	local debugInfo = {}
+	debugInfo.type = "Object Debug Info"
+	debugInfo.id = objectId
+	debugInfo.objectClassId, debugInfo.objectClass = EntityUtility.getObjectClass(objectId)
+	debugInfo.object = g_currentMission:getNodeObject(objectId) or "nil"
+	debugInfo.rigidBodyType = getRigidBodyType(objectId)
+	debugInfo.index = EntityUtility.nodeToIndex(objectId, self.mapNode)
+	debugInfo.name = getName(objectId)
+	debugInfo.clipDistance = getClipDistance(objectId)
+	debugInfo.mask = getObjectMask(objectId)
+
+	if debugInfo.objectClassId == ClassIds.SHAPE then
+		debugInfo.isNonRenderable = getIsNonRenderable(objectId)
+		debugInfo.geometry = getGeometry(objectId)
+		debugInfo.material = getMaterial(objectId, 0)
+		debugInfo.materialName = getName(debugInfo.material)
+	end
+
+	return debugInfo
+end
+
+---@param objectId integer
+---@return integer | nil
+---@return string
+function MapObjectsHider:getRealHideObject(objectId)
+	-- local amo = self.animatedMapObjectCollisions[objectId]
+	-- if amo ~= nil then
+		-- return amo.mapObjectsHider.rootNode, getName(amo.mapObjectsHider.rootNode)
+	-- end
+
+	-- try to intercept big sized objects with LOD such as houses
+	if getName(getParent(objectId)) == "LOD0" then
+		local rootNode = getParent(getParent(objectId))
+		return rootNode, getName(rootNode)
+	end
+
+	local name = ""
+	local id = nil
+
+	-- try to intercept medium sized objects such as electric cabins
+	local parent = getParent(objectId)
+	if getNumOfChildren(objectId) <= 8 then
+		EntityUtility.queryNodeHierarchy(
+			parent,
+			function(_, nodeName, depth)
+				if depth == 2 then
+					if string.find(string.lower(nodeName), "decal", 1, true) then
+						name = getName(parent)
+						id = parent
+					end
+				end
+			end
+		)
+		if id ~= nil then
+			return id, name
+		end
+	end
+
+	EntityUtility.queryNodeParents(
+		objectId,
+		function(node, nodeName)
+			-- do some extra checks to ensure that's the real object
+			if getVisibility(node) then
+				id = node
+				name = nodeName
+				return false
+			end
+			return true
+		end
+	)
+	return id, name
+end
+
+---@param objectId integer
+---@param name string
+---@param hiderPlayerName string
+function MapObjectsHider:hideObject(objectId, name, hiderPlayerName)
+	if g_server ~= nil then
+		local objectName = name or getName(objectId)
+
+		local object = MapObjectsHider:getHideObject(objectId, objectName, hiderPlayerName)
+
+		if MapObjectsHider:checkHideObject(object) then
+			self:hideNode(object.id)
+			HideDecollideNodeEvent.sendToClients(object.index, true)
+			for _, collision in pairs(object.collisions) do
+				self:decollideNode(collision.id)
+				HideDecollideNodeEvent.sendToClients(collision.index, false)
+			end
+			table.insert(self.hiddenObjects, object)
+		end
+	else
+		ObjectHideRequestEvent.sendToServer(objectId)
+	end
+end
+
+---@param objectId integer
+---@param objectName string
+---@param hiderPlayerName string
+---@return HideObject
+function MapObjectsHider:getHideObject(objectId, objectName, hiderPlayerName)
+	---@class HideObject
+	local object = {}
+	object.index = EntityUtility.nodeToIndex(objectId, self.mapNode)
+	object.id = objectId
+	object.hash = EntityUtility.getNodeHierarchyHash(objectId, self.mapNode, self.md5)
+	object.name = objectName
+	object.date = getDate("%d/%m/%Y")
+	object.time = getDate("%H:%M:%S")
+	object.timestamp = Utility.getTimestamp()
+	object.player = hiderPlayerName or g_currentMission.userManager:getUserByUserId(g_currentMission.player.userId):getNickname()
+
+	---@type HideObjectCollision[]
+	object.collisions = {}
+	EntityUtility.queryNodeHierarchy(
+		objectId,
+		---@param node integer
+		---@param name string
+		function(node, name)
+			local rigidType = getRigidBodyType(node)
+			if rigidType ~= "NoRigidBody" then
+				---@class HideObjectCollision
+				local col = {}
+				col.index = EntityUtility.nodeToIndex(node, self.mapNode)
+				col.name = name
+				col.id = node
+				col.rigidBodyType = rigidType
+				table.insert(object.collisions, col)
+			end
+		end
+	)
+	return object
+end
+
+---@param object HideObject
+---@return boolean
+function MapObjectsHider:checkHideObject(object)
+    if type(object.id) ~= "number" or not entityExists(object.id) then
+        return false
+    end
+
+    if object.hash ~= EntityUtility.getNodeHierarchyHash(object.id, self.mapNode, self.md5) then
+        return false
+    end
+
+    if object.name ~= getName(object.id) then
+        return false
+    end
+
+    for _, collision in pairs(object.collisions) do
+        if type(collision.id) ~= "number" or not entityExists(collision.id) then
+            return false
+        end
+
+        if collision.rigidBodyType ~= getRigidBodyType(collision.id) then
+            return false
+        end
+
+        if collision.name ~= getName(collision.id) then
+            return false
+        end
+    end
+
+    return true
+end
+---@param nodeId integer
+function MapObjectsHider:hideNode(nodeId)
+    setVisibility(nodeId, false)
+end
+
+---@param nodeId integer
+function MapObjectsHider:decollideNode(nodeId)
+    setRigidBodyType(nodeId, RigidBodyType.NONE)
 end
 
 -- function MapObjectsHider:update(dt)
---
+++ scripts/playerExtension.lua
@@ -0,0 +1,200 @@
+--- Map Objects Hider
+
+---@author Duke of Modding
+---@version 1.2.0.0
+---@date 29/11/2020
+
+PlayerExtension = {}
+
+---@param superFunc function
+---@param isServer boolean
+---@param isClient boolean
+---@return Player
+function PlayerExtension.new(isServer, isClient)
+	self = Player.originalNew(isServer, isClient)
+	self.inputInformation.registrationList[InputAction.MAP_OBJECT_HIDER_HIDE] = {
+		eventId = "",
+		callback = self.hideObjectActionEvent,
+		triggerUp = false,
+		triggerDown = true,
+		triggerAlways = false,
+		activeType = Player.INPUT_ACTIVE_TYPE.STARTS_ENABLED,
+		callbackState = nil,
+		text = g_i18n:getText("moh_HIDE"),
+		textVisibility = true
+	}
+	self.inputInformation.registrationList[InputAction.MAP_OBJECT_HIDER_GUI] = {
+		eventId = "",
+		callback = self.showHiddenObjectsListActionEvent,
+		triggerUp = false,
+		triggerDown = true,
+		triggerAlways = false,
+		activeType = Player.INPUT_ACTIVE_TYPE.STARTS_ENABLED,
+		callbackState = nil,
+		text = g_i18n:getText("moh_MAP_OBJECT_HIDER_GUI"),
+		textVisibility = true
+	}
+	Logging.info("PlayerExtension new called")
+	return self
+end
+
+---@param superFunc function
+---@param dt number
+function PlayerExtension:update(superFunc, dt)
+	superFunc(self, dt)
+	if MapObjectsHider.debug and self.debugInfo ~= nil then
+		DebugUtility.renderTable(0.05, 0.98, 0.009, self.debugInfo, 3, false)
+	end
+	if MapObjectsHider.debug and self.hideObjectDebugInfo ~= nil then
+		DebugUtility.renderTable(0.35, 0.98, 0.009, self.hideObjectDebugInfo, 3, false)
+	end
+end
+
+---@param superFunc function
+---@param dt number
+function PlayerExtension:updateTick(superFunc, dt)
+	superFunc(self, dt)
+	if self.isEntered and g_dedicatedServerInfo == nil then
+		local x, y, z = localToWorld(self.cameraNode, 0, 0, 1.0)
+		local dx, dy, dz = localDirectionToWorld(self.cameraNode, 0, 0, -1)
+		if self.raycastHideObject ~= nil then
+			self.lastRaycastHideObject = self.raycastHideObject
+		end
+		self.raycastHideObject = nil
+		self.debugInfo = nil
+		self.hideObjectDebugInfo = nil
+		raycastAll(x, y, z, dx, dy, dz, "raycastCallback", 5, self)
+	end
+end
+
+---@param hitObjectId integer
+---@return boolean
+function PlayerExtension:raycastCallback(hitObjectId)
+	if hitObjectId ~= self.rootNode then
+		if getHasClassId(hitObjectId, ClassIds.SHAPE) then
+			if hitObjectId == self.lastRaycastHitObject and not MapObjectsHider.debug then
+				self.raycastHideObject = self.lastRaycastHideObject
+				return false
+			end
+			local objectFound = false
+			if MapObjectsHider.debug and self.debugInfo == nil then
+				-- debug first hitted object
+				self.debugInfo = MapObjectsHider:getObjectDebugInfo(hitObjectId)
+			end
+			local rigidBodyType = getRigidBodyType(hitObjectId)
+			if (rigidBodyType == RigidBodyType.STATIC or rigidBodyType == RigidBodyType.DYNAMIC) then
+				if getSplitType(hitObjectId) ~= 0 then
+					self.raycastHideObject = {name = getName(getParent(hitObjectId)), objectId = hitObjectId, isSplitShape = true}
+					if MapObjectsHider.debug then
+						-- debug placeable
+						self.hideObjectDebugInfo = {type = "Split Type", splitType = g_splitTypeManager:getSplitTypeByIndex(getSplitType(hitObjectId))}
+					end
+					objectFound = true
+				elseif g_currentMission:getNodeObject(hitObjectId) == nil then
+					local object = {}
+					object.id, object.name = MapObjectsHider:getRealHideObject(hitObjectId)
+					if object.id ~= nil then
+						self.raycastHideObject = object
+						if MapObjectsHider.debug then
+							-- debug hide object
+							self.hideObjectDebugInfo = MapObjectsHider:getObjectDebugInfo(object.id)
+						end
+						objectFound = true
+					end
+				else
+					local object = g_currentMission:getNodeObject(hitObjectId)
+					if object:isa(Placeable) then
+						local storeItem = g_storeManager:getItemByXMLFilename(object.configFileName)
+						if storeItem ~= nil then
+							self.raycastHideObject = {name = storeItem.name, object = object, isSellable = true}
+							if MapObjectsHider.debug then
+								-- debug placeable
+								self.hideObjectDebugInfo = {type = "Placeable", storeItem = storeItem}
+							end
+							objectFound = true
+						end
+					end
+				end
+			end
+			if objectFound then
+				self.lastRaycastHitObject = hitObjectId
+				return false
+			end
+		end
+	end
+	return true -- continue raycast
+end
+
+---@param superFunc function
+function PlayerExtension:updateActionEvents(superFunc)
+	superFunc(self)
+	if self.raycastHideObject ~= nil then
+		local id = self.inputInformation.registrationList[InputAction.MAP_OBJECT_HIDER_HIDE].eventId
+		if self.raycastHideObject.isSellable then
+			g_inputBinding:setActionEventText(id, g_i18n:getText("moh_SELL"):format(self.raycastHideObject.name))
+		elseif self.raycastHideObject.isSplitShape then
+			g_inputBinding:setActionEventText(id, g_i18n:getText("moh_DELETE"):format(self.raycastHideObject.name))
+		else
+			g_inputBinding:setActionEventText(id, g_i18n:getText("moh_HIDE"):format(self.raycastHideObject.name))
+		end
+		g_inputBinding:setActionEventActive(id, true)
+		g_inputBinding:setActionEventTextVisibility(id, true)
+	else
+		local id = self.inputInformation.registrationList[InputAction.MAP_OBJECT_HIDER_HIDE].eventId
+		g_inputBinding:setActionEventActive(id, false)
+		g_inputBinding:setActionEventTextVisibility(id, false)
+	end
+	local id = self.inputInformation.registrationList[InputAction.MAP_OBJECT_HIDER_GUI].eventId
+	g_inputBinding:setActionEventTextVisibility(id, MapObjectsHider.guiShowHelpEnabled)
+end
+
+function PlayerExtension:hideObjectActionEvent()
+	if self.raycastHideObject ~= nil then
+		self.raycastHideObjectBackup = self.raycastHideObject
+		if self.raycastHideObject.isSellable then
+			if MapObjectsHider.sellConfirmEnabled then
+				g_gui:showYesNoDialog({text = g_i18n:getText("moh_sell_dialog_text"):format(self.raycastHideObjectBackup.name), title = g_i18n:getText("moh_dialog_title"), callback = self.sellObjectDialogCallback, target = self})
+			else
+				self:sellObjectDialogCallback(true)
+			end
+		elseif self.raycastHideObject.isSplitShape then
+			if MapObjectsHider.deleteSplitShapeConfirmEnabled then
+				g_gui:showYesNoDialog({text = g_i18n:getText("moh_delete_split_shape_dialog_text"), title = g_i18n:getText("moh_dialog_title"), callback = self.deleteSplitShapeDialogCallback, target = self})
+			else
+				self:deleteSplitShapeDialogCallback(true)
+			end
+		else
+			if MapObjectsHider.hideConfirmEnabled then
+				g_gui:showYesNoDialog({text = g_i18n:getText("moh_dialog_text"):format(self.raycastHideObjectBackup.name), title = g_i18n:getText("moh_dialog_title"), callback = self.hideObjectDialogCallback, target = self})
+			else
+				self:hideObjectDialogCallback(true)
+			end
+		end
+	end
+end
+
+function PlayerExtension:showHiddenObjectsListActionEvent()
+	MapObjectsHider:openGui()
+end
+
+---@param yes boolean
+function PlayerExtension:hideObjectDialogCallback(yes)
+	if yes and self.raycastHideObjectBackup ~= nil and self.raycastHideObjectBackup.id ~= nil then
+		MapObjectsHider:hideObject(self.raycastHideObjectBackup.id)
+		self.raycastHideObjectBackup = nil
+	end
+end
+
+---@param yes boolean
+function PlayerExtension:sellObjectDialogCallback(yes)
+	if yes and self.raycastHideObjectBackup ~= nil and self.raycastHideObjectBackup.object ~= nil then
+		g_client:getServerConnection():sendEvent(SellPlaceableEvent:new(self.raycastHideObjectBackup.object))
+	end
+end
+
+---@param yes boolean
+function PlayerExtension:deleteSplitShapeDialogCallback(yes)
+	if yes and self.raycastHideObjectBackup ~= nil and self.raycastHideObjectBackup.objectId ~= nil then
+		g_client:getServerConnection():sendEvent(DeleteSplitShapeEvent:new(self.raycastHideObjectBackup.objectId))
+	end
+end
