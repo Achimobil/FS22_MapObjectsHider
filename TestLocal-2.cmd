51504d2705ab0d43829377e74d2cc876d73d2f3a
Achim Kuntz <achimobil@hotmail.com>
2023-08-22 12:52:13 +0200

    cmd's angepasst

--- TestLocal-2.cmd
+++ TestLocal-2.cmd
@@ -1,2 +1,2 @@
-"C:\Program Files\7-Zip\7z.exe" a -tzip "E:\LS22Profile\LS22ModTest\mods\FS22_MapObjectsHider.zip" -x@FileDefinition.txt
+"C:\Program Files\7-Zip\7z.exe" u -tzip "E:\LS22Profile\LS22ModTest\mods\FS22_MapObjectsHider.zip" -x@FileDefinition.txt
 start "" "C:\Program Files (x86)\Farming Simulator 2022\x64\FarmingSimulator2022Game.exe" -autoStartSavegameId 2 -cheats -profile E:\LS22Profile\LS22ModTest -devWarning
